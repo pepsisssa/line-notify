8ab30a5f78f2e090b81e2fbec8aa26b7c62b696b
Aki <segutaide@gmail.com>
2024-01-05 01:03:55 +0900

    ログ設定の改良

--- main.py
+++ main.py
@@ -4,6 +4,28 @@ from bs4 import BeautifulSoup
 
 # ログのフォーマットを設定
 log_format = '%(levelname)s : %(asctime)s : %(message)s'
-logging.basicConfig(filename='path/log.log', level=logging.INFO, format=log_format)
-logging.info('START')  # ログの開始を記録
 
+# ロギング設定を行う関数を作成
+def configure_logging(log_file_path, log_level=logging.INFO):
+    # ルートロガーを取得
+    logger = logging.getLogger()
+
+    # ログのフォーマットを設定
+    formatter = logging.Formatter(log_format)
+
+    # コンソールハンドラを作成して設定
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+
+    # ファイルハンドラを作成して設定
+    file_handler = logging.FileHandler(log_file_path)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
+    # ログレベルを設定
+    logger.setLevel(log_level)
+
+# ロギングの設定を行う関数を呼び出し
+configure_logging('path/log.log')
+logging.info('START')
