9b93ffcd537c867015bb400159e3396a20e3006e
Aki <segutaide@gmail.com>
2024-01-05 00:31:07 +0900

    アクセストークンの追加

--- linenotify.py
+++ linenotify.py
@@ -4,7 +4,7 @@ def main():
     send_message_line_notify('新しいコード 312395773002。')
 
 def send_message_line_notify(message):
-    line_notify_token = 'lhJWSltbTmeL0vHJX2vJQq9ZZzAzaEMcRzHLi4yOeKC'
+    line_notify_token = 'アクセストークン'
     line_notify_api = 'https://notify-api.line.me/api/notify'
     headers = {'Authorization': 'Bearer ' + line_notify_token}
     data = {'message': message}
