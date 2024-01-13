90af0257ef6c16f8b83ab6c0de57a0d519cd9c13
Aki <segutaide@gmail.com>
2024-01-14 03:51:08 +0900

    ログ設定の短縮

--- main.py
+++ main.py
@@ -1,31 +1,10 @@
 import requests
-import logging
 from bs4 import BeautifulSoup
+import logging  
 
-# ログのフォーマットを設定
-log_format = '%(levelname)s : %(asctime)s : %(message)s'
+URL = "https://scraping-for-beginner.herokuapp.com/"
+LAST_HREF = ''
 
-# ロギング設定を行う関数を作成
-def configure_logging(log_file_path, log_level=logging.INFO):
-    # ルートロガーを取得
-    logger = logging.getLogger()
-
-    # ログのフォーマットを設定
-    formatter = logging.Formatter(log_format)
-
-    # コンソールハンドラを作成して設定
-    console_handler = logging.StreamHandler()
-    console_handler.setFormatter(formatter)
-    logger.addHandler(console_handler)
-
-    # ファイルハンドラを作成して設定
-    file_handler = logging.FileHandler(log_file_path)
-    file_handler.setFormatter(formatter)
-    logger.addHandler(file_handler)
-
-    # ログレベルを設定
-    logger.setLevel(log_level)
-
-# ロギングの設定を行う関数を呼び出し
-configure_logging('path/log.log')
-logging.info('START')
+# ロギングの設定
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
