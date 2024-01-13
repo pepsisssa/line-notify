aa645c482e80de5b5818a957d7f6c33dda36862c
Aki <segutaide@gmail.com>
2024-01-14 03:36:32 +0900

    scheduleを使用し定期的に実行できるよう改良

--- linenotify.py
+++ linenotify.py
@@ -1,16 +1,25 @@
-import requests
+import schedule
+import time
+from main import check_for_updates
+import logging  
+
+LINE_NOTIFY_TOKEN = 'lhJWSltbTmeL0vHJX2vJQq9ZZzAzaEMcRzHLi4yOeKC'
+
+# ロギングの設定
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def job():
+    check_for_updates(LINE_NOTIFY_TOKEN)
 
 def main():
-    send_message_line_notify('新しいコード 312395773002。')
+    job()  # 初回のチェック
+    schedule.every(5).minutes.do(job)
 
-def send_message_line_notify(message):
-    line_notify_token = 'lhJWSltbTmeL0vHJX2vJQq9ZZzAzaEMcRzHLi4yOeKC'
-    line_notify_api = 'https://notify-api.line.me/api/notify'
-    headers = {'Authorization': 'Bearer ' + line_notify_token}
-    data = {'message': message}
-    requests.post(line_notify_api, headers=headers, data=data)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
 
 if __name__ == "__main__":
     main()
-
 
