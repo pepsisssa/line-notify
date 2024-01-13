8506abfd02e782c3dfe138957e3fa9d8b0e92e3b
Aki <segutaide@gmail.com>
2024-01-14 03:52:35 +0900

    関数 Line Notify を使いメッセージを送信する

--- main.py
+++ main.py
@@ -8,3 +8,9 @@ LAST_HREF = ''
 # ロギングの設定
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+def send_line_notify(notification_message, line_notify_token):
+    line_notify_api = 'https://notify-api.line.me/api/notify'
+    headers = {'Authorization': f'Bearer {line_notify_token}'}
+    data = {'message': f'{notification_message}'}
+    requests.post(line_notify_api, headers=headers, data=data)
