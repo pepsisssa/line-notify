41f7d26932b6143c57a168f7a83433893425e03c
Aki <segutaide@gmail.com>
2024-01-15 23:53:00 +0900

    コメントを追加

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import logging  
 
+# スクレイピング対象のURL
 URL = "https://scraping-for-beginner.herokuapp.com/"
 LAST_HREF = ''
 
@@ -9,12 +10,14 @@ LAST_HREF = ''
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+# Lineに通知を送信するための関数
 def send_line_notify(notification_message, line_notify_token):
     line_notify_api = 'https://notify-api.line.me/api/notify'
     headers = {'Authorization': f'Bearer {line_notify_token}'}
     data = {'message': f'{notification_message}'}
     requests.post(line_notify_api, headers=headers, data=data)
 
+# ウェブサイトの更新を確認するための関数
 def check_for_updates(line_notify_token):
     global LAST_HREF
     try:
@@ -33,5 +36,6 @@ def check_for_updates(line_notify_token):
                 logger.info(f"Notification sent: {title} {URL}{href}")
             LAST_HREF = href
 
+# リクエストの例外が発生した場合はログに記録
     except requests.exceptions.RequestException as e:
         logger.error(f"Web scraping error: {e}")
